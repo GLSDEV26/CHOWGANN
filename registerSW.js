83b6e1f5c972cb3e0b472e2f3951f809fb40ead2
GLSDEV26 <GLSDEV26@users.noreply.github.com>
2026-03-01 00:10:54 +0000

    deploy: dca904633e24bf3863221499872b08e51eb8a21c

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/CHOWGANN/sw.js', { scope: '/CHOWGANN/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
